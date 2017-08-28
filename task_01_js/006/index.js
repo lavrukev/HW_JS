49d632cdcb0bdac62e91f8d8a7d2db654563d198
Vladimir Lavrukevich <lavrukevich.v@gmail.com>
2017-08-28 22:02:07 +0300

    added fixes

--- task_01_js/006/index.js
+++ task_01_js/006/index.js
@@ -1,6 +1,14 @@
 function calculation () {
 	var a = document.getElementById('one').value;
+	// var b = document.getElementById('two').value;
 	var p = document.getElementById('out');
+	// var t1=b*0.1;
+	// var t2=b*0.2;
+	// var t3=b*0.3;
+
+	// document .getElementById('tax').innerHTML = t1;
+	// document .getElementById('tax').innerHTML = t2;
+	// document .getElementById('tax').innerHTML = t3;
 
 	
 	if (a<3 && a>=0) {
@@ -8,12 +16,16 @@ function calculation () {
 	}
 	else if (a>=3 && a<10) {
 		p.innerHTML = 'Ваша надбавка составляет 10%';
+		// t.innerHTML = 'Итого'
 	}
 	else if (a>=10 && a<20) {
 		p.innerHTML = 'Ваша надбавка составляет 20%';
 	}
 	else if (a>=20 && a<30) {
 		p.innerHTML = 'Ваша надбавка составляет 30%';
+	}
+	else if (a>50) {
+		p.innerHTML = 'Вы реально Терминатор!';
 	}
 	else if (a<0) {
 		p.innerHTML = 'Вы ввели неправильные данные - отрицательное число';
