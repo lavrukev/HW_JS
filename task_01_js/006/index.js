d9077fac71b47dac8a6764b647e3884bf923e887
Vladimir Lavrukevich <lavrukevich.v@gmail.com>
2017-08-29 19:21:56 +0300

    added files for 6th hw

--- task_01_js/006/index.js
+++ task_01_js/006/index.js
@@ -1,28 +1,23 @@
 function calculation () {
 	var a = document.getElementById('one').value;
-	// var b = document.getElementById('two').value;
+	var b = document.getElementById('two').value;
 	var p = document.getElementById('out');
-	// var t1=b*0.1;
-	// var t2=b*0.2;
-	// var t3=b*0.3;
-
-	// document .getElementById('tax').innerHTML = t1;
-	// document .getElementById('tax').innerHTML = t2;
-	// document .getElementById('tax').innerHTML = t3;
-
+	var z = document.getElementById('tax');
 	
 	if (a<3 && a>=0) {
 		p.innerHTML = 'Ваша надбавка составляет 0%';
 	}
 	else if (a>=3 && a<10) {
 		p.innerHTML = 'Ваша надбавка составляет 10%';
-		// t.innerHTML = 'Итого'
+		z.innerHTML = 'Сумма надбавки ' + (b*0.1);
 	}
 	else if (a>=10 && a<20) {
 		p.innerHTML = 'Ваша надбавка составляет 20%';
+		z.innerHTML = 'Сумма надбавки ' + (b*0.2);
 	}
 	else if (a>=20 && a<30) {
 		p.innerHTML = 'Ваша надбавка составляет 30%';
+		z.innerHTML = 'Сумма надбавки ' + (b*0.3);
 	}
 	else if (a>50) {
 		p.innerHTML = 'Вы реально Терминатор!';
